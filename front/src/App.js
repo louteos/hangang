9252c7e991c49b2ee455343a1d99878cc3ec2db1
louteos <gdismw@gmail.com>
2023-01-12 17:13:03 +0900

    db아키텍처설계

---
+++ front/src/App.js
@@ -0,0 +1,25 @@
+import Header from './component/header/Header';
+import Main from './component/Main';
+import Intro from './component/Introduce';
+import History from './component/History';
+import Incubating from './component/Incubating';
+import Interview from './component/Interview';
+import Notice from './component/Notice';
+import News from './component/News';
+
+function App() {
+  return (
+    <div>
+    <Header></Header>
+    <Main></Main>
+    <Intro></Intro>
+    <History></History>
+    <Incubating></Incubating>
+    <Interview></Interview>
+    <Notice></Notice>
+    <News></News>
+    </div>
+  );
+}
+
+export default App;
